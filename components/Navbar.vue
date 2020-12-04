41028a699e3296488ed688ef9d78277fca694ef0
d <d@>
2020-12-04 16:38:23 +0300

    Navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Elatra</a>
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">For Entrepreneurs </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Start a Campaign </a>
+          </li>
+          <li class="nav-item float-right">
+            <a class="nav-link" href="#">Log In</a>
+          </li>
+          <li class="nav-item float-left">
+            <a class="nav-link" href="#">Sign Up</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="container">
+  <div class="container-fluid">
+    <Navbar />
     <div>
       <Logo />
       <h1 class="title">elatra</h1>
@@ -30,15 +31,6 @@ export default {}
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
 .title {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
